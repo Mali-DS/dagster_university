66917fc5d5dde762d1cf149c2135ddcb684ebe4b
malishariat <44557358+Mali-DS@users.noreply.github.com>
2024-08-26 13:00:46 +1200

    test

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -2,14 +2,21 @@ import requests
 from . import constants
 from dagster import asset
 
+import ssl
+import certifi
+from urllib.request import urlopen
+
+
 @asset
 def taxi_trips_file() -> None:
     """
       The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
     """
     month_to_fetch = '2023-03'
+    request = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    urlopen(request, context=ssl.create_default_context(cafile=certifi.where()))
     raw_trips = requests.get(
-        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+        request   
     )
 
     with open(
